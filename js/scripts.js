ff5ef13432ae77b5328b5b7b058a9df585d91a1c
Hayder Hassan <hayderh@gmail.com>
2019-03-11 21:00:33 +0000

    Make images responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $( document ).ready(function() {
+    updateGridWidth();
     $('.ui.dropdown').dropdown();
+
+    $(window).resize(function() {
+      updateGridWidth();
+    });
+
 });
+
+function updateGridWidth() {
+  if ($(window).width() < 700) {
+    setGridWidth("one");
+  } else if ($(window).width() < 1250) {
+    setGridWidth("two");
+  } else if ($(window).width() < 1600) {
+    setGridWidth("three");
+  } else {
+   setGridWidth("four");
+  }
+}
+
+function setGridWidth(size) {
+  $('#main-grid').removeClass('one two three four column grid');
+  $('#main-grid').addClass(size + ' column grid');
+  return;
+}
